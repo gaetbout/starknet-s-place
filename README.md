f09aa9967d882253eecc074ee17359a45852ed80
gaetbout <gaetan.ansotte@hotmail.com>
2022-05-21 16:42:20 +0200

    first draft testing time ~10s ==> 200*300

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # starknet-r-place
+![Tests](https://github.com/gaetbout/starknet-r-place/actions/workflows/python-app.yml/badge.svg)
+
+TODO description
+
+## 🌡️ Test
+
+*Prerequisite - Have a working cairo environment.*  
+To run the test suite, copy this repository and put yourself at the root.  
+Compile the contracts using `make build` or `nile compile`.  
+Run the tests using `make test` or, for more details, `pytest -v`.   
+For more  details check the Actions tab of this GitHub repository.
+
+
+# TODO
+
+## 📄 License
+
+**starknet-r-place** is released under the [MIT](LICENSE).
+
+
+
+
---
+++ tests/test_storage.py
@@ -0,0 +1,31 @@
+import os
+
+import time
+import pytest
+from starkware.starknet.testing.starknet import Starknet
+
+CONTRACT_FILE = os.path.join("contracts", "Storage.cairo")
+
+@pytest.fixture(scope="session")
+async def contract(starknet):
+    return await starknet.deploy(source=CONTRACT_FILE,)
+  
+@pytest.mark.asyncio
+async def test_view_get_up_to(contract ):
+    numbers = []
+    print("\nStarting generation")
+    print(time.time())
+    limit = 1666
+    for x in range(limit):
+        numbers.append(x)
+        await contract.at_put(x, x).invoke()
+    print("\nGetting view generation")
+    ts1 = time.time()
+    print(ts1)
+    execution_info = await contract.view_get_up_to(limit).invoke()
+    assert execution_info.result.arr == numbers
+    print("\nEnd")
+    ts2  = time.time()
+    print(ts2)
+    print ("\nActual elapsed time (s): " + str(ts2 - ts1) )
+  
