e74777bac5b367d5ed5e627ed63af66e92f9f714
gaetbout <gaetan.ansotte@hotmail.com>
2022-05-31 20:29:00 +0200

    try fixing test action

--- tests/utils.py
+++ tests/utils.py
@@ -3,7 +3,10 @@ from starkware.starkware_utils.error_handling import StarkException
 from starkware.starknet.business_logic.state.state import BlockInfo
 from starkware.starknet.business_logic.execution.objects import Event
 from starkware.starknet.public.abi import get_selector_from_name
-
+from starkware.starknet.definitions.general_config import (
+    DEFAULT_GAS_PRICE,
+    DEFAULT_SEQUENCER_ADDRESS
+)
 async def assert_revert(fun, reverted_with=None):
     try:
         await fun
@@ -16,7 +19,7 @@ async def assert_revert(fun, reverted_with=None):
 
 def set_block_timestamp(starknet_state, block_timestamp):
     starknet_state.block_info = BlockInfo(
-        block_number=starknet_state.block_info.block_number, block_timestamp=block_timestamp, gas_price=starknet_state.block_info.gas_price)
+        block_number=starknet_state.block_info.block_number, block_timestamp=block_timestamp, gas_price=DEFAULT_GAS_PRICE, sequencer_address=DEFAULT_SEQUENCER_ADDRESS)
 
 def assert_event_emitted(tx_exec_info, from_address, name, data):
     assert Event(
