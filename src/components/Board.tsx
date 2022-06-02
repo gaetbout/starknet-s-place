16998aa0d396012056da11cb61ddba8f75f1d81a
gaetbout <gaetan.ansotte@hotmail.com>
2022-06-02 20:16:17 +0200

    megaboard

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,16 +6,6 @@ import { useSPlaceContract } from '~/hooks/s_place'
 export function Board() {
     const { contract } = useSPlaceContract()
 
-    function sliceArray(obj) {
-        let array = (obj as number[])[0]
-        const chunkSize = 100;
-        let arr = [];
-        for (let i = 0; i < array.length; i += chunkSize) {
-            const chunk = array.slice(i, i + chunkSize);
-            arr.push(chunk);
-        }
-        return arr.toString()
-    }
     const { data: board1Result } = useStarknetCall({
         contract,
         method: 'view_get_board',
@@ -34,30 +24,17 @@ export function Board() {
         args: [220, 103],
     })
 
-    const board1Value = useMemo(() => {
-        if (board1Result && board1Result.length > 0) {
-            return sliceArray(board1Result)
+    const megaBoard = useMemo(() => {
+        if (board1Result && board1Result.length > 0 && board2Result && board2Result.length > 0 && board3Result && board3Result.length > 0) {
+            return (board1Result.arr.concat(board2Result.arr).concat(board3Result.arr))
         }
-    }, [board1Result])
+    }, [board1Result, board2Result, board3Result])
 
-    const board2Value = useMemo(() => {
-        if (board2Result && board2Result.length > 0) {
-            // console.log("2:" + board2Result)
-            return board2Result.toString()
-        }
-    }, [board2Result])
-
-    const board3Value = useMemo(() => {
-        if (board3Result && board3Result.length > 0) {
-            // console.log("3:" + board3Result)
-            return board3Result.toString()
-        }
-    }, [board3Result])
     return (
-        <div>
-            <div>{board1Value}</div>
-            <div>{board2Value}</div>
-            <div>{board3Value}</div>
+        <div style={{ width: '1200px', marging: '0px' }}>
+            {megaBoard?.map((val, index) => (
+                <div style={{ display: 'inline-block', width: '12px', height: '12px' }} key={index}>{val.toString()}</div>
+            ))}
         </div>
     )
 }
