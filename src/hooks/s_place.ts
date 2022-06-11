37046be98e4b20482c09d375ff37631eaca72926
gaetbout <gaetan.ansotte@hotmail.com>
2022-06-11 17:51:13 +0200

    redeploy contract

--- src/hooks/s_place.ts
+++ src/hooks/s_place.ts
@@ -6,6 +6,6 @@ import RPlace from '~/abi/s_place.json'
 export function useSPlaceContract() {
     return useContract({
         abi: RPlace as Abi,
-        address: '0x000565a06870d1afb94785b865d04942f74ac301de111310fb546ccd64b92a19',
+        address: '0x01345318950b5d02f5f2f31e8bca08884c5aea389646fbfea61dcdfdc9402260',
     })
 }
