988373e55d1339423dadea45aa06ca97677577fc
gaetbout <gaetan.ansotte@hotmail.com>
2022-06-18 13:34:32 +0200

    Update test_storage.py

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -8,24 +8,25 @@ CONTRACT_FILE = os.path.join("contracts", "storage.cairo")
 @pytest.fixture(scope="session")
 async def contract(starknet):
     return await starknet.deploy(source=CONTRACT_FILE,)
-  
-@pytest.mark.asyncio
-async def test_view_get_up_to(contract ):
-    # Used  for tests purposes
-    numbers = []
-    print("\nStarting generation")
-    print(time.time())
-    limit = 100
-    for x in range(limit):
-        numbers.append(x)
-        await contract.at_put(x, x).invoke()
-    print("\nGetting view generation")
-    ts1 = time.time()
-    print(ts1)
-    execution_info = await contract.view_get_up_to(limit).invoke()
-    assert execution_info.result.arr == numbers
-    print("\nEnd")
-    ts2  = time.time()
-    print(ts2)
-    print ("\nActual elapsed time (s): " + str(ts2 - ts1) )
+
+# Test done to assess loading time 
+# @pytest.mark.asyncio
+#async def test_view_get_up_to(contract ):
+#    # Used  for tests purposes
+#    numbers = []
+#    print("\nStarting generation")
+#    print(time.time())
+#    limit = 100
+#    for x in range(limit):
+#        numbers.append(x)
+#        await contract.at_put(x, x).invoke()
+#    print("\nGetting view generation")
+#    ts1 = time.time()
+#    print(ts1)
+#    execution_info = await contract.view_get_up_to(limit).invoke()
+#    assert execution_info.result.arr == numbers
+#    print("\nEnd")
+#    ts2  = time.time()
+#    print(ts2)
+#    print ("\nActual elapsed time (s): " + str(ts2 - ts1) )
   
